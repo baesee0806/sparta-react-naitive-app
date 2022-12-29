29880d41d0fb616b2d2cc50c1eb3f785d1059c39
baesee <baesee@kakao.com>
2022-12-29 19:29:00 +0900

    fontawesom으로 icon and emotion으로 스타일컴퍼넌트 줬음

--- App.js
+++ App.js
@@ -1,41 +1,124 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text } from "react-native";
+import styled from "@emotion/native";
+import { useState } from "react";
+import { FontAwesome } from "@expo/vector-icons";
 
 export default function App() {
+  const [text, onChangeText] = useState("");
+  const [toggleBtn, setToggleBtn] = useState({
+    name: "",
+    onPress: false,
+  });
+
+  const toggleHanler = (name) => {
+    setToggleBtn({ ...toggleBtn, name: name, onPress: true });
+  };
+
   return (
-    <View style={styles.container}>
-      <View style={styles.separator}>
-        <Button title='Javascript'/>
-      </View>
-      <View style={styles.separator}>
-        <Button title='React'/> 
-      </View>
-      <View style={styles.separator}>
-        <Button title='Coding Test'/>
-      </View>
-    
-    </View>
+    <StView>
+      <StBtnBox>
+        <StToggleBtn background={toggleBtn.name === "Javascript" && toggleBtn.onPress ? "yellow" : "gray"} onPress={() => toggleHanler("Javascript")}>
+          <Text>Javascript</Text>
+        </StToggleBtn>
+        <StToggleBtn background={toggleBtn.name === "React" && toggleBtn.onPress ? "yellow" : "gray"} onPress={() => toggleHanler("React")}>
+          <Text>React</Text>
+        </StToggleBtn>
+        <StToggleBtn background={toggleBtn.name === "Coding Test" && toggleBtn.onPress ? "yellow" : "gray"} onPress={() => toggleHanler("Coding Test")}>
+          <Text>Coding Test</Text>
+        </StToggleBtn>
+      </StBtnBox>
+      <StTextInput placeholder="할일을 입력해주세요" onChangeText={onChangeText} value={text} />
+      <StatusBar style="auto" />
+      <StScrollView>
+        <TodoList>
+          <Text>신나는 실행컨텍스트 공부</Text>
+          <TDLBtnBox>
+            <TDLBtn>
+              <FontAwesome name="check-square" size={33} color="black" />
+            </TDLBtn>
+            <TDLBtn>
+              <FontAwesome name="pencil-square" size={33} color="black" />
+            </TDLBtn>
+            <TDLBtn>
+              <FontAwesome name="trash" size={33} color="black" />
+            </TDLBtn>
+          </TDLBtnBox>
+        </TodoList>
+        <TodoList>
+          <Text>너무 좋은 ES6 최신문법 공부</Text>
+          <TDLBtnBox>
+            <TDLBtn>
+              <FontAwesome name="check-square" size={33} color="black" />
+            </TDLBtn>
+            <TDLBtn>
+              <FontAwesome name="pencil-square" size={33} color="black" />
+            </TDLBtn>
+            <TDLBtn>
+              <FontAwesome name="trash" size={33} color="black" />
+            </TDLBtn>
+          </TDLBtnBox>
+        </TodoList>
+      </StScrollView>
+    </StView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection:"row",
-    fontSize : "5",
-  },
-  separator: {
-        flexDirection:"row",
-        marginVertical: 8,
-        width: 100,
-        height: 50,
-        justifyContent: 'center',
-        borderWidth: 1,
-        borderRadius: 5,
-        borderStyle: 'solid',
-        
-  },
-});
+const StView = styled.SafeAreaView`
+  width: 100%;
+  align-items: center;
+`;
+
+const StBtnBox = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 90%;
+  height: 100px;
+  margin-top: 30px;
+  border-bottom-width: 2px;
+`;
+
+const StToggleBtn = styled.TouchableOpacity`
+  width: 110px;
+  height: 60px;
+  background: ${(x) => x.background};
+  justify-content: center;
+  align-items: center;
+`;
+
+const StTextInput = styled.TextInput`
+  width: 90%;
+  height: 50px;
+  padding: 10px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  border: 1px solid black;
+`;
+
+const StScrollView = styled.ScrollView`
+  width: 90%;
+  height: 70%;
+  border-top-width: 2px;
+`;
+
+const TodoList = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding-left: 10px;
+  margin-top: 20px;
+  width: 100%;
+  height: 50px;
+  background-color: #d9d9d9;
+`;
+
+const TDLBtnBox = styled.View`
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TDLBtn = styled.TouchableOpacity`
+  margin-right: 8px;
+`;
